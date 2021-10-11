c5462ccd8aa072f4b420f89903366e76fed7c659
ViniBavaresco <91674067+ViniBavaresco@users.noreply.github.com>
2021-10-11 18:00:16 -0300

    TextoAtualizado0.1

--- src/Projeto3/cadastroUsuario.java
+++ src/Projeto3/cadastroUsuario.java
@@ -1,11 +1,22 @@
 package Projeto3;
-
+import java.util.Scanner;
 public class cadastroUsuario {
-
 	public static void main(String[] args) {
+		Scanner leia = new Scanner (System.in);
+		String nomeUsuario, cpfUsuario;
+		int idadeUsuario;
 		// Fazer a captura de informações do usuário
 		System.out.println("Digite aqui seu nome completo:"
 		+" (Apenas letras maiúsculas e minúsculas, sem símbolos ou números)");	
+		nomeUsuario = leia.nextLine();
+		System.out.println("Digite a sua idade: ");
+		idadeUsuario= leia.nextInt();
+		System.out.println("Digite o seu CPF(Cadastro de Pessoas Físicas): (Somente números)");
+		cpfUsuario = leia.next();
+		
+		System.out.println("\nNome: "+nomeUsuario);
+		System.out.println("Idade: "+idadeUsuario+" anos");
+		System.out.println("CPF: "+cpfUsuario);
+		leia.close();
 	}
-
 }
