d3d36b904b1a36a5e9042a373a54d22c0d53165f
ViniBavaresco <91674067+ViniBavaresco@users.noreply.github.com>
2021-10-11 16:57:58 -0300

    Inicio de Projeto
    Primeiro projeto Java iniciado, junto com o primeiro pacote e outras classes

---
+++ src/Projeto3/cadastroUsuario.java
@@ -0,0 +1,11 @@
+package Projeto3;
+
+public class cadastroUsuario {
+
+	public static void main(String[] args) {
+		// Fazer a captura de informações do usuário
+		System.out.println("Digite aqui seu nome completo:"
+		+" (Apenas letras maiúsculas e minúsculas, sem símbolos ou números)");	
+	}
+
+}
---
+++ src/Projeto3/inicioNomePrograma.java
@@ -0,0 +1,10 @@
+package Projeto3;
+
+public class inicioNomePrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
