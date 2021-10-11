dd08da0cb52f93b404cdc96c42a01c897e3d993a
Beatriz <90809864+BeaSouzaa@users.noreply.github.com>
2021-10-11 20:02:06 -0300

    Update informacoesUsuario.java

--- src/Projeto3/informacoesUsuario.java
+++ src/Projeto3/informacoesUsuario.java
@@ -4,6 +4,59 @@ public class informacoesUsuario {
 	private String cpfUsuario;
 	private int idadeUsuario;
 	
+	
+	 ArrayList<String> lista = new ArrayList<String>();
+		        
+		        lista.add("\nAciclovir");
+		        lista.add("\nÁcido valpróico");
+		        lista.add("\nAlbendazol");
+		        lista.add("\nAmitriptilina");
+		        lista.add("\nAmoxicilina");
+		        lista.add("\nAnlodipino");
+		        lista.add("\nBenzilpenicilina benzatina");
+		        lista.add("\nBenzilpenicilina procaína + benzilpenicilina potássica");
+		        lista.add("\nBiperideno");
+		        lista.add("\nCaptopril");
+		        lista.add("\nCarbamazepina");
+		        lista.add("\nCarbonato de Lítio");
+		        lista.add("\nCefalexina");
+		        lista.add("\nClomipramina");
+		        lista.add("\nClonazepam");
+		        lista.add("\nClorpromazina");
+		        lista.add("\nDexametasona");
+		        lista.add("\nDiazepam");
+		        lista.add("\nDigoxina");
+		        lista.add("\nDipirona");
+		        lista.add("\nEnalapril");
+		        lista.add("\nFenitoína");
+		        lista.add("\nFenobarbital");
+		        lista.add("\nFurosemida");
+		        lista.add("\nFluoxetina");
+		        lista.add("\nGlibenclamida");
+		        lista.add("\nHaloperidol");
+		        lista.add("\nHidroclorotiaziada");
+		        lista.add("\nHidróxido de Alumínio");
+		        lista.add("\nIbuprofeno");
+		        lista.add("\nLoratadina");
+		        lista.add("\nMetformina");
+		        lista.add("\nMetildopa");
+		        lista.add("\nMetronidazol");
+		        lista.add("\nMiconazol");
+		        lista.add("\nNitrofutantoína");
+		        lista.add("\nOmeprazol");
+		        lista.add("\nParacetamol");
+		        lista.add("\nPrometazina");
+		        lista.add("\nPropranolol");
+		        lista.add("\nRivotril");
+		        lista.add("\nSais para reidratação oral");
+		        lista.add("\nSalbutamol");
+		        lista.add("\nSertralina");
+		        lista.add("\nSulfanetoxazol + Trimetroprima");
+		        lista.add("\nSertralina");
+		         
+	
+	
+	
 	public String getNomeUsuario() {
 		return nomeUsuario;
 	}
